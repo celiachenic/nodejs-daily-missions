5cc0ac39373dc59b4965ef2279437465f33333a0
msnigatsu <msnigatsu@gmail.com>
2026-07-06 23:33:46 +0800

    初始化 Express 主程式

---
+++ 16-auth/app.js
@@ -0,0 +1,30 @@
+const path = require("node:path");
+require("dotenv").config({
+  path: path.join(__dirname, ".env"),
+});
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const authMiddleware = require("./middleware/auth");
+const auth = require("./routes/auth");
+const notes = require("./routes/notes");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", auth);
+app.use("/note", authMiddleware, notes);
+
+app.use((req, res) => {
+  res.status(404).json({ status: "error", message: "404：此頁面不存在" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ status: "error", message: "500：內部伺服器錯誤" });
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
